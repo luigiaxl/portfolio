052f28ea5e420f1e94109cc6291853ca91573635
luigiaxl <luigi.axl.wagas@gmail.com>
2020-04-11 17:41:23 +0800

    Initial layout.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div
+    id="app"
+    class="h-full flex flex-col md:flex-row"
+  >
+    <AboutMe class="w-full md:w-2/6"></AboutMe>
+
+    <div class="flex-1 py-10">
+      <div>
+        <router-link :to="{name: 'home'}">Home</router-link> |
+        <router-link :to="{name: 'projects'}">Projects</router-link> |
+        <router-link :to="{name: 'skills'}">Skills</router-link> |
+        <router-link :to="{name: 'about'}">About Me</router-link>
+      </div>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import AboutMe from './components/about_me/AboutMe.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  const components = {
+    AboutMe
   }
-}
+
+  export default {
+    components
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    Projects
   </div>
 </template>
